a8645120db4864860835a4f78dc1298ee575e80a
HemanthKumarPenubothu <120077353+HemanthKumarPenubothu@users.noreply.github.com>
2023-05-22 21:22:28 +0530

    #include<stdio.h> int main() {     int i,a,sum=0;     scanf("%d",&a);     for(i=1;i<a;i++)     {         if(a%i==0)         sum=sum+i;
        }
        if(sum==a)
        printf("True");
        else
        printf("False");
    }

---
+++ Perfect_Number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,a,sum=0;
+    scanf("%d",&a);
+    for(i=1;i<a;i++)
+    {
+        if(a%i==0)
+        sum=sum+i;
+        
+    }
+    if(sum==a)
+    printf("True");
+    else
+    printf("False");
+}
