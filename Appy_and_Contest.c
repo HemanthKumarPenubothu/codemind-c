c26314086db148d1e9df5783fde6da50d45d776c
HemanthKumarPenubothu <120077353+HemanthKumarPenubothu@users.noreply.github.com>
2023-05-24 17:30:31 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     while(t--)     {         int n,a,b,k,c=0;         scanf("%d%d%d%d",&n,&a,&b,&k);         if(a%b==0)         c=a;         else if(b%a==0)         c=b;         else         c=a*b;         int d,e,f;         d=n/c;         e=n/a;         f=n/b;         e=e-d;         f=f-d;         if(e+f>=k)         {             printf("Win ");         }         else         {             printf("Lose ");         }     } }

--- Appy_and_Contest.c
+++ Appy_and_Contest.c
@@ -1,18 +1,33 @@
 #include<stdio.h>
 int main()
 {
-    int tc,n,a,b,k,c,i;
-    scanf("%d",&tc);
-    for(i=1;i<=tc;i++)
+    int t;
+    scanf("%d",&t);
+    while(t--)
     {
+        int n,a,b,k,c=0;
         scanf("%d%d%d%d",&n,&a,&b,&k);
-        c=(n/a)+(n/b);
-        if(c>=k)
-        printf("Win
-");
+        if(a%b==0)
+        c=a;
+        else if(b%a==0)
+        c=b;
         else
-        printf("Lose
+        c=a*b;
+        int d,e,f;
+        d=n/c;
+        e=n/a;
+        f=n/b;
+        e=e-d;
+        f=f-d;
+        if(e+f>=k)
+        {
+            printf("Win
 ");
+        }
+        else
+        {
+            printf("Lose
+");
+        }
     }
-    
 }
