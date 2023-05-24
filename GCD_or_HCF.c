eb54937d5b2439c5fe31d5080a2aea72be9d8ff4
HemanthKumarPenubothu <120077353+HemanthKumarPenubothu@users.noreply.github.com>
2023-05-24 10:25:56 +0530

    #include<stdio.h> int main() {     int i,a,b,hcf;     scanf("%d%d",&a,&b);     for(i=1;i<b;i++)     {         if(a%i==0 && b%i==0)         hcf=i;     }     printf("%d",hcf); }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,i,hcf;
+    int i,a,b,hcf;
     scanf("%d%d",&a,&b);
-    for(i=1;i<=a && i<=b;i++)
+    for(i=1;i<b;i++)
     {
         if(a%i==0 && b%i==0)
-        {
-            hcf=i;
-        }
+        hcf=i;
     }
     printf("%d",hcf);
 }
