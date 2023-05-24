adb2591115e3ec9b7f3320c88c5feb2c9768812f
HemanthKumarPenubothu <120077353+HemanthKumarPenubothu@users.noreply.github.com>
2023-05-24 10:04:55 +0530

    #include<stdio.h> int main() {     int i,n,c=0;     scanf("%d", &n);     if(n==1 || n==0)     {         printf("True");         c=1;     }
        for(i=2;i<=n/2;i++)
        {
            if(n==i*i)
            {
                printf("True");
                c=1;
                break;
            }
        }
        if(c==0)
        printf("False");
        
    
    }

--- perfect_square_root_or_not.c
+++ perfect_square_root_or_not.c
@@ -3,7 +3,13 @@ int main()
 {
     int i,n,c=0;
     scanf("%d", &n);
-    for(i=1;i<=n/2;i++)
+    if(n==1 || n==0)
+    {
+        printf("True");
+        c=1;
+    }
+
+    for(i=2;i<=n/2;i++)
     {
         if(n==i*i)
         {
