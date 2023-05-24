4ef544321e5cf9cb33efb21b717b54c6bfeb8bd9
HemanthKumarPenubothu <120077353+HemanthKumarPenubothu@users.noreply.github.com>
2023-05-24 11:22:45 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=n-1;i>=n/2;i--)     {         printf("%d ",a[i]);     }     for(i=0;i<n/2;i++)     {         printf("%d ",a[i]);     }
    }

---
+++ Half_List.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=n-1;i>=n/2;i--)
+    {
+        printf("%d ",a[i]);
+    }
+    for(i=0;i<n/2;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    
+}
