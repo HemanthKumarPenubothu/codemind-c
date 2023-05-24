5c27d719772d520fc8d2293aaacd484567491b93
HemanthKumarPenubothu <120077353+HemanthKumarPenubothu@users.noreply.github.com>
2023-05-24 09:38:54 +0530

    #include<stdio.h> #include<string.h> int main() {     char a[100];     scanf("%[^ ]s",a);     int i,j,l=strlen(a),c=0;     for(i=0;i<l;i++)     {         for(j=0;j<l;j++)         {             if(a[i]==a[j] && i!=j)             {                 c++;             }         }     }     if(c==0)     printf("Unique Number");     else     printf("Not Unique Number"); }

--- Unique_number.c
+++ Unique_number.c
@@ -5,23 +5,18 @@ int main()
     char a[100];
     scanf("%[^
 ]s",a);
-    int i,j,l=strlen(a),c,z;
+    int i,j,l=strlen(a),c=0;
     for(i=0;i<l;i++)
     {
-        c=0;
         for(j=0;j<l;j++)
         {
-            if(i!=j && a[i]==a[j])
+            if(a[i]==a[j] && i!=j)
             {
                 c++;
             }
         }
-        if(c==0)
-        {
-            z++;
-        }
     }
-    if(z==l)
+    if(c==0)
     printf("Unique Number");
     else
     printf("Not Unique Number");
