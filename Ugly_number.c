30772eab4274ac073310e2a016276861ffd1aef3
HemanthKumarPenubothu <120077353+HemanthKumarPenubothu@users.noreply.github.com>
2023-05-24 12:46:23 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     while(n!=1)     {         if(n%5==0)         {             n=n/5;         }         else if(n%2==0)         {             n=n/2;         }         else if(n%3==0)         {             n=n/3;         }         else         {             printf("Not Ugly Number");             return 0;         }
        }
        printf("Ugly Number");
    }

---
+++ Ugly_number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n!=1)
+    {
+        if(n%5==0)
+        {
+            n=n/5;
+        }
+        else if(n%2==0)
+        {
+            n=n/2;
+        }
+        else if(n%3==0)
+        {
+            n=n/3;
+        }
+        else
+        {
+            printf("Not Ugly Number");
+            return 0;
+        }
+        
+    }
+    printf("Ugly Number");
+}
